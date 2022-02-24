4c0cd31549b0f2953ff635259675d15895c84f49
BingfanTian96 <90421854+BingfanTian96@users.noreply.github.com>
2022-02-24 04:27:35 -0800

    Update

--- PM2/cargo_create_tables.sql
+++ PM2/cargo_create_tables.sql
@@ -21,24 +21,24 @@ DROP TABLE IF EXISTS Companies;
 
 
 CREATE TABLE Companies (
- CompanyId INT AUTO_INCREMENT,
- CompanyName VARCHAR(255),
- Description VARCHAR(1023),
+ CompanyId 		INT 	AUTO_INCREMENT,
+ CompanyName 	VARCHAR(255),
+ Description 	VARCHAR(1023),
  CONSTRAINT pk_Companies_CompanyId PRIMARY KEY (CompanyId)
 );
 
 CREATE TABLE Users (
- UserId INT AUTO_INCREMENT,
- FirstName VARCHAR(255) NOT NULL,
- LastName VARCHAR(255) NOT NULL,
- Email VARCHAR(255) NOT NULL,
+ UserId 		INT 	AUTO_INCREMENT,
+ FirstName 		VARCHAR(255) 	NOT NULL,
+ LastName 		VARCHAR(255)	NOT NULL,
+ Email 			VARCHAR(255) 	NOT NULL,
  Password VARCHAR(255) NOT NULL,
  CONSTRAINT pk_Users_UserId PRIMARY KEY (UserId),
  CONSTRAINT uq_Users_Email UNIQUE(Email)
 );
 
 CREATE TABLE Admins (
- UserId INT,
+ UserId 	INT,
  CONSTRAINT pk_Admins_UserId PRIMARY KEY (UserId),
  CONSTRAINT fk_Admins_UserId FOREIGN KEY (UserId)
   REFERENCES Users(UserId)
@@ -46,9 +46,9 @@ CREATE TABLE Admins (
 );
 
 CREATE TABLE Buyers (
- BuyerId INT,
- DOB DATE NOT NULL,
- ZIP INT NOT NULL,
+ BuyerId 		INT,
+ DOB 			DATE 	NOT NULL,
+ ZIP 			INT 	NOT NULL,
  CONSTRAINT pk_Admins_BuyerId PRIMARY KEY (BuyerId),
  CONSTRAINT fk_Admins_BuyerId FOREIGN KEY (BuyerId)
   REFERENCES Users(UserId)
@@ -56,11 +56,11 @@ CREATE TABLE Buyers (
 );
 
 CREATE TABLE Sellers (
- SellerId INT,
- CompanyId INT,
- ZIP INT NOT NULL,
- Address VARCHAR(255) NOT NULL,
- Introduction VARCHAR(1023),
+ SellerId 		INT,
+ CompanyId 		INT,
+ ZIP 			INT 			NOT NULL,
+ Address 		VARCHAR(255) 	NOT NULL,
+ Introduction 	VARCHAR(1023),
  CONSTRAINT pk_Sellers_SellerId PRIMARY KEY (SellerId),
  CONSTRAINT fk_Sellers_SellerId FOREIGN KEY (SellerId)
   REFERENCES Users(UserId)
@@ -72,60 +72,60 @@ CREATE TABLE Sellers (
 
 
 create table Searches(
-    SearchId int auto_increment,
-    Year int,
-    Make varchar(255),
-    Model varchar(255),
-    Trim varchar(255),
-    Body varchar(255),
-    Transmission varchar(255),
-    State varchar(255),
-    Odometer int,
-    CarCondition double,
-    Color varchar(255),
-    Interior varchar(255),
-    UserId int,
+    SearchId 		int 	auto_increment,
+    Year 			int,
+    Make 			varchar(255),
+    Model 			varchar(255),
+    Trim 			varchar(255),
+    Body 			varchar(255),
+    Transmission 	varchar(255),
+    State 			varchar(255),
+    Odometer 		int,
+    CarCondition 	double,
+    Color 			varchar(255),
+    Interior 		varchar(255),
+    UserId 			int,
     constraint pk_Search_SearchId primary key (SearchId),
     constraint fk_Search_UserId foreign key (UserId) references Buyers(BuyerId)
 );
 
 create table Cars(
-    Vin varchar(255),
-    Year int,
-    Make varchar(255),
-    Model varchar(255),
-    Trim varchar(255),
-    Body varchar(255),
-    Transmission varchar(255),
-    State varchar(255),
-    Odometer int,
-    CarCondition double,
-    Color varchar(255),
-    Interior varchar(255),
-    Mmr int,
-    SellerId int,
+    Vin 			varchar(255)	Not null,
+    Year 			int,
+    Make 			varchar(255),
+    Model 			varchar(255),
+    Trim 			varchar(255),
+    Body 			varchar(255),
+    Transmission 	varchar(255),
+    State 			varchar(255),
+    Odometer 		int,
+    CarCondition 	double,
+    Color 			varchar(255),
+    Interior 		varchar(255),
+    Mmr 			int,
+    SellerId 		int,
     constraint pk_Search_Vin primary key (Vin),
     constraint fk_Search_SellerId foreign key (SellerId) references Sellers(SellerId)
 );
 
 create table Reviews(
-    ReviewId int auto_increment,
-    Date timestamp,
-    ReviewContent varchar(255),
-    Rating double,
-    BuyerId int,
-    SellerId int,
+    ReviewId 		int 	auto_increment,
+    Date 			timestamp,
+    ReviewContent 	varchar(255),
+    Rating 			double,
+    BuyerId 		int,
+    SellerId 		int,
     constraint pk_Review_ReviewId PRIMARY KEY (ReviewId),
     constraint fk_Review_BuyerId foreign key (BuyerId) references Buyers(BuyerId),
     constraint fk_Review_SellerId foreign key (SellerId) references Sellers(SellerId)
 );
 
 CREATE TABLE Messages (
-	MessageId INT AUTO_INCREMENT,
-	SentTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	Content VARCHAR(1023) NOT NULL,
-	FromId INT,
-	ToId INT,
+	MessageId 		INT 			AUTO_INCREMENT,
+	SentTime 		TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP 	NOT NULL,
+	Content 		VARCHAR(1023) 	NOT NULL,
+	FromId 			INT,
+	ToId 			INT,
 	CONSTRAINT pk_Messages_MessageId PRIMARY KEY(MessageId),
 	CONSTRAINT fk_Messages_FromId FOREIGN KEY(FromId) REFERENCES Users(UserId)
 	ON UPDATE CASCADE ON DELETE SET NULL,
@@ -134,9 +134,9 @@ CREATE TABLE Messages (
 );
 
 CREATE TABLE Saves(
-	SaveId INT AUTO_INCREMENT,
-	Vin VARCHAR(255),
-	UserId INT,
+	SaveId 		INT 	AUTO_INCREMENT,
+	Vin 		VARCHAR(255),
+	UserId 		INT,
 	CONSTRAINT pk_Saves_SaveId PRIMARY KEY(SaveId),
 	CONSTRAINT fk_Saves_Vin FOREIGN KEY(Vin) REFERENCES Cars(Vin)
 	ON UPDATE CASCADE ON DELETE CASCADE,
