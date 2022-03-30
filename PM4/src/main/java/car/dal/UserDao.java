59b3cffad53692e0d1b68b0ba1c5da0fdada60b2
chenbonikko <90677844+chenbonikko@users.noreply.github.com>
2022-03-29 21:00:25 -0700

    Update UserDao.java

---
+++ PM4/src/main/java/car/dal/UserDao.java
@@ -0,0 +1,150 @@
+package car.dal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import car.model.*;
+/**
+ * 
+ * @author Bo Chen
+ *
+ */
+public class UserDao{
+	protected ConnectionManager connectionManager;
+	
+	// Single pattern: instantiation is limited to one object.
+	private static UserDao instance = null;
+	protected UserDao() {
+		connectionManager = new ConnectionManager();
+	}
+	public static UserDao getInstance() {
+		if(instance == null) {
+			instance = new UserDao();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Save the Users instance by storing it in your MySQL instance.
+	 * This runs a INSERT statement.
+	 */
+	public User create(User user) throws SQLException {
+		String insertUser = "INSERT INTO User(FirstName,LastName,Email,Password) " +
+        "VALUES(?,?,?,?);";
+		Connection connection = null;
+		PreparedStatement insertStmt = null;
+        ResultSet resultKey = null;
+		try {
+			connection = connectionManager.getConnection();
+			insertStmt = connection.prepareStatement(insertUser);
+			// PreparedStatement allows us to substitute specific types into the query template.
+			// For an overview, see:
+			// http://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html.
+			// http://docs.oracle.com/javase/7/docs/api/java/sql/PreparedStatement.html
+			// For nullable fields, you can check the property first and then call setNull()
+			// as applicable.
+			insertStmt.setString(1, user.getFirstName());
+			insertStmt.setString(2, user.getLastName());
+			insertStmt.setString(3, user.getEmail());
+			insertStmt.setString(4, user.getPassword());
+
+			// Note that we call executeUpdate(). This is used for a INSERT/UPDATE/DELETE
+			// statements, and it returns an int for the row counts affected (or 0 if the
+			// statement returns nothing). For more information, see:
+			// http://docs.oracle.com/javase/7/docs/api/java/sql/PreparedStatement.html
+			// I'll leave it as an exercise for you to write UPDATE/DELETE methods.
+			insertStmt.executeUpdate();
+			// Retrieve the auto-generated key and set it, so it can be used by the caller.
+			resultKey = insertStmt.getGeneratedKeys();
+			int userId = -1;
+			if(resultKey.next()) {
+				userId = resultKey.getInt(1);
+			} else {
+				throw new SQLException("Unable to retrieve auto-generated key.");
+			}
+			user.setUserId(userId);
+			return user;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(insertStmt != null) {
+				insertStmt.close();
+			}
+		}
+	}
+	
+	public User getUserByUserName(String userName) throws SQLException {
+		String selectUser = "SELECT UserName,Password,FirstName,LastName,Email,Phone FROM Users WHERE UserName=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectUser);
+			selectStmt.setString(1, userName);
+			// Note that we call executeQuery(). This is used for a SELECT statement
+			// because it returns a result set. For more information, see:
+			// http://docs.oracle.com/javase/7/docs/api/java/sql/PreparedStatement.html
+			// http://docs.oracle.com/javase/7/docs/api/java/sql/ResultSet.html
+			results = selectStmt.executeQuery();
+			// You can iterate the result set (although the example below only retrieves 
+			// the first record). The cursor is initially positioned before the row.
+			// Furthermore, you can retrieve fields by name and by type.
+			if(results.next()) {
+				String resultUserName = results.getString("UserName");
+				String password = results.getString("Password");
+				String firstName = results.getString("FirstName");
+				String lastName = results.getString("LastName");
+				String email = results.getString("Email");
+				String phone = results.getString("Phone");
+				User user = new User(resultUserName, password, firstName, lastName, email, phone);
+				return user;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return null;
+	}
+	
+	public User delete(User user) throws SQLException {
+		String deleteUser = "DELETE FROM Users WHERE UserId=?;";
+		Connection connection = null;
+		PreparedStatement deleteStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			deleteStmt = connection.prepareStatement(deleteUser);
+			deleteStmt.setInt(1, user.getUserId());
+			deleteStmt.executeUpdate();
+
+			// Return null so the caller can no longer operate on the Persons instance.
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(deleteStmt != null) {
+				deleteStmt.close();
+			}
+		}
+	}
+}
